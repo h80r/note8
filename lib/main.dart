e420cbedafbe0a1720d6f4ca2b9c2ab415d34696
h80r <h80rgbc@gmail.com>
2022-01-13 10:39:03 -0300

    Save state with Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'canvas/home.dart';
 import 'canvas/settings.dart';
 
-void main() {
+void main() async {
+  Hive.init((await getApplicationDocumentsDirectory()).path);
+  await Hive.openBox<bool>('settings');
+
   runApp(
     // The ProviderScope widget saves the state of all Providers, so it needs to
     // be at the top of your Widget tree.
--- lib/provider/settings.dart
+++ lib/provider/settings.dart
@@ -1,13 +1,20 @@
+import 'package:hive/hive.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SettingsNotifier extends StateNotifier<bool> {
-  SettingsNotifier() : super(false);
+  SettingsNotifier(this._settings)
+      : super(_settings.get('isDark', defaultValue: false)!);
 
   static final provider = StateNotifierProvider<SettingsNotifier, bool>((ref) {
-    return SettingsNotifier();
+    return SettingsNotifier(Hive.box<bool>('settings'));
   });
 
   static final notifier = provider.notifier;
 
-  void setDark(bool isDark) => state = isDark;
+  final Box<bool> _settings;
+
+  void setDark(bool isDark) async {
+    await _settings.put('isDark', isDark);
+    state = isDark;
+  }
 }
